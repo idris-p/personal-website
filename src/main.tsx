c30e3b82ffd5daa359228c3efd9e95cc0ad9738a
Idris Popoola <idris.popoola@student.manchester.ac.uk>
2026-08-31 00:41:11 +0100

    Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
+import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+
+inject();
+injectSpeedInsights();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
